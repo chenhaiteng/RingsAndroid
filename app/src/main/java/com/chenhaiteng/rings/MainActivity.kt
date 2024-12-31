2957dce2372af8e44318a7ef23fdeab388ad4a76
Chen-Hai Teng <chenhaiteng@gmail.com>
2024-12-31 13:31:18 +0800

    Implement demo app

--- app/src/main/java/com/chenhaiteng/rings/MainActivity.kt
+++ app/src/main/java/com/chenhaiteng/rings/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.chenhaiteng.rings.ui.theme.RingsAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,9 +35,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+    RingText(
+        modifier = Modifier.fillMaxSize(),
+        name,
+        "A་BC་A་B་C་A་B་C་A་B་C་A་B་C་A་B་C".split("་").map {
+            RingTextComponent(it, Color.Red, TextStyle(fontSize = 40.sp))
+        }.toTypedArray(),
+        fontRatio = 0.2f,
+        insetRatio = 0.2f,
+        outline = 5f,
+        outlineColor = Color.White,
+        showBlueprint = true
     )
 }
 
